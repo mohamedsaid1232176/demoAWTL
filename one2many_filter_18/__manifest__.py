d5626aafccf84c0fa5278ea04be77a5721f28d2c
Mohamed Said <moamedsaid1234@gmail.com>
2026-06-09 16:34:48 +0300

    add

---
+++ one2many_filter_18/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "One2many Filter 18",
+    "version": "1.0",
+    "author": "ChatGPT",
+    "category": "Tools",
+    "summary": "Filter one2many lines with AND/OR logic",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "one2many_filter_18/static/src/js/one2many_filter.js",
+            "one2many_filter_18/static/src/xml/one2many_filter.xml"
+        ]
+    },
+    "installable": True,
+    "application": True,
+    "price": 50.0,
+    "currency": "USD",
+    "images": ["static/description/banner.png"],
+}
